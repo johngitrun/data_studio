6658baa95526fd5d2700b1daf336758d65c98897
johngitrun <john0581+github@gmail.com>
2026-06-08 11:21:55 +0800

    Update dbo.TableB.sql

--- Tables/dbo.TableB.sql
+++ Tables/dbo.TableB.sql
@@ -3,7 +3,8 @@ SET QUOTED_IDENTIFIER ON
 
 CREATE TABLE [dbo].[TableB] (
 	[ID] int,
-	[Name] varchar(50) COLLATE Chinese_Taiwan_Stroke_CI_AS
+	[Name] varchar(50) COLLATE Chinese_Taiwan_Stroke_CI_AS,
+	[Notes] varchar(200)
 ) ON [PRIMARY]
 CREATE STATISTICS [_WA_Sys_00000001_239E4DCF]
 ON [dbo].[TableB] ([ID])
